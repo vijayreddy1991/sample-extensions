be4d2967635e0216fc74c1ee3718b47e0f0d39dc
Bharath Upadhya <1992bharath@gmail.com>
2020-07-14 16:56:02 +0530

    Delete onInput.sh

--- resources/sampleResource/1.0.0/onInput.sh
+++
@@ -1,7 +0,0 @@
-sampleResource() {
-  local resourceName="$1"
-  local resourceFoo=$(find_resource_variable "$resourceName" "foo")
-  echo "resourceFoo: $resourceFoo"
-}
-
-execute_command "sampleResource %%context.resourceName%%"
