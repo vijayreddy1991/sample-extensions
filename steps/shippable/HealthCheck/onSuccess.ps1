429ba1b820b6fd5c24c6f65a73cbdd4d4d6d2358
Rahul Jain <54938495+rahulnlb1@users.noreply.github.com>
2021-01-18 12:20:03 +0530

    Update onSuccess.ps1

--- steps/shippable/HealthCheck/onSuccess.ps1
+++ steps/shippable/HealthCheck/onSuccess.ps1
@@ -3,4 +3,10 @@ Function Get-Integration-Name() {
   Write-Output $IntegrationName
 }
 
+Function Get-Resource-Name() {
+  $ResourceName = get_resource_name -ResourceNamespace "jfrog" -ResourceType "GitRepo" -ResourceOperation "IN" -ResourceVersion "v1.0"
+  Write-Output $ResourceName
+}
+
 execute_command Get-Integration-Name
+execute_command Get-Resource-Name
